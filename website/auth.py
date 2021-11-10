021b227584431ca93a8b486f6a409d4267bb311d
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-11-10 10:50:25 +0300

    imported flaskand created sign up function

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text= "Testing")
+    
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    
-     return render_template("sign_up.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get("password1")
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # user can now be added to database
+            pass
+
+    return render_template("sign_up.html")
